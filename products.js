8ecf2a162abd1b64d6a66cd7aa72b4c0ce0d77cb
Sahl97 <149163967+Sahl97@users.noreply.github.com>
2023-11-20 22:07:12 +0400

    Week 7

---
+++ products.js
@@ -0,0 +1,57 @@
+let products = [
+    { 
+        "id": 1001,
+        "title": "Cat Food, 25lb bag",
+        "description": "A 25 Pound bag of <em> irresistable </em>, organic goodness for your cat.",
+        "price": 2000,
+        "image": Image/image1.jpg,
+        "stock": 10,
+        "availableInventory": 10,
+        "showProduct": true,
+        "rating": 3
+    },
+    { 
+        "id": 1002,
+        "title": "Dog Food, 30lb bag",
+        "description": "A 30 Pound bag of nutritious and delicious dog food.",
+        "price": 2500,
+        "image": Image/image2.png,
+        "stock": 15,
+        "availableInventory": 15,
+        "showProduct": true,
+        "rating": 4
+    },
+    { 
+        "id": 1003,
+        "title": "Fish Tank",
+        "description": "A beautiful fish tank with colorful fish and accessories.",
+        "price": 1500,
+        "image": "fishtank.jpg",
+        "stock": 5,
+        "availableInventory": 5,
+        "showProduct": true,
+        "rating": 5
+    },
+    { 
+        "id": 1004,
+        "title": "Bird Cage",
+        "description": "A spacious and comfortable cage for your feathered friend.",
+        "price": 1200,
+        "image": "birdcage.jpg",
+        "stock": 8,
+        "availableInventory": 8,
+        "showProduct": true,
+        "rating": 4
+    },
+    { 
+        "id": 1005,
+        "title": "Hamster Wheel",
+        "description": "An exercise wheel for your energetic hamster.",
+        "price": 500,
+        "image": "hamsterwheel.jpg",
+        "stock": 20,
+        "availableInventory": 20,
+        "showProduct": true,
+        "rating": 3
+    },
+];
